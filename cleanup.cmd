329f048d6671dff45f9a306bbba632a129b3c309
Maciej Misztal <mmisztal1980@gmail.com>
2017-10-19 12:09:33 +0200

    front-page img added

--- cleanup.cmd
+++
@@ -1,6 +0,0 @@
-docker-compose down
-docker-compose rm --force
-docker rmi demo-app-web demo-app-db-migrator
-rd /s ./artifacts/
-rd /s ./packages/
-rd /s ./obj/
--- scripts/connect-to-cluster.bat
+++
@@ -1,2 +0,0 @@
-az account set --subscription 7278e08f-cad5-416a-b0b4-cf3fb5f8b270
-az acs kubernetes get-credentials --resource-group=netdevday-acs-rgp --name=netdevday
